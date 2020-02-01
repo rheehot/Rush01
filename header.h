bbd3fbf581d4d1b6ec9b18a162c5b3e080ca2cd0
mimseong <parkseongmin98@gmail.com>
2020-02-01 16:41:41 +0900

    Update header.h
    안 적은 함수 추가함

--- header.h
+++ header.h
@@ -6,19 +6,25 @@
 /*   By: seongpar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/01 10:38:20 by seongpar          #+#    #+#             */
-/*   Updated: 2020/02/01 10:38:25 by seongpar         ###   ########.fr       */
+/*   Updated: 2020/02/01 16:35:46 by seongpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_H
 # define HEADER_H
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
 
-int	ft_stack_box(int index, int *udlr[], int *ans);
-int ft_check(int index, int *udlr[], int *ans);
+int		ft_argc_check(int argc);
+void    ft_atoi(char *str, int **udlr);
+int		ft_check(int index, int *udlr[], int *ans);
+int		ft_handle_exception(int argc, char *argv[]);
+int		ft_length_check(char **argv);
 void	ft_print_box(int *ans);
 void	ft_putchar(char c);
+int		ft_range_check(char **argv);
+int		ft_shape_check(char **argv);
+int		ft_stack_box(int index, int *udlr[], int *ans);
 
 #endif
